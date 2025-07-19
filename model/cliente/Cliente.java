890bb832c69531790ddda6f54abca4ac369114af
yanrsa-dev <yanrochadesa10@gmail.com>
2025-07-19 19:48:50 -0300

    Update Cliente.java

--- model/cliente/Cliente.java
+++ model/cliente/Cliente.java
@@ -1,4 +1,6 @@
 package model.cliente;
+import java.util.ArrayList;
+import java.util.List;
 /*• Cadastrar cliente com identificador único, nome, endereço e telefone.
 • Listar clientes.
 • Atualizar clientes.*/
@@ -33,7 +35,17 @@ public abstract class Cliente {
   public void setEndereco(String endereco) {
     this.endereco = endereco;
   }
-  
+  //criando a lista de clientes
+   private List<Cliente> clientes = new ArrayList<>();
+  //cadastrando os clientes
+  public CLiente cadastraCliente(String telefone,String nome, String endereco){
+    Cliente cliente= new Cliente(telefone,nome,endereco);
+    clientes.add(cliente);
+    return cliente;
+  }
+  public List<Cliente> listarClientes() {
+        return clientes;
+    }
 }
 
   
