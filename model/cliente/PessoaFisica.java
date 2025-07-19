75f2d80e20ebe890454764afd481cab2eeb4a688
yanrsa-dev <yanrochadesa10@gmail.com>
2025-07-19 19:25:03 -0300

    Update PessoaFisica.java

--- model/cliente/PessoaFisica.java
+++ model/cliente/PessoaFisica.java
@@ -1,6 +1,20 @@
 package model.cliente;
 
-public class PessoaFisica {
+public class PessoaFisica extends Cliente{
+  private String cpf;
+
+  public PessoaFisica(){
+    super(nome,preco, quantidadeEmEstoque);
+    this.peso=peso;
+  }
+
+  public double getPeso() {
+    return peso;
+  }
+
+  public void setPeso(double peso) {
+    this.peso = peso;
+  }
 
   
 }
