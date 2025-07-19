3386aa7930aa0f9943110f26adaa514cdf5b3438
yanrsa-dev <yanrochadesa10@gmail.com>
2025-07-19 19:28:58 -0300

    Update PessoaJuridica.java

--- model/cliente/PessoaJuridica.java
+++ model/cliente/PessoaJuridica.java
@@ -1,6 +1,18 @@
 package model.cliente;
 
-public class PessoaJuridica {
+public class PessoaJuridica extends Cliente{
+  private String cnpj;
+  public PessoaJuridica(String telefone, String nome, String endereco,String cnpj){
+    super(telefone,nome, endereco,cnpj);
+    this.cnpj=cnpj;
+  }
+  public String getCnpj() {
+    return cnpj;
+  }
+
+  public void setCnpj(String cnpj) {
+    this.cnpj = cnpj;
+  }
 
   
 }
