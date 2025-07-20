7f419ffa25197a151af8a7f5eaa34e674987b1dc
yanrsa-dev <yanrochadesa10@gmail.com>
2025-07-20 20:50:11 -0300

    Update ClienteController.java

--- controller/ClienteController.java
+++ controller/ClienteController.java
@@ -16,7 +16,7 @@ private ClienteService clienteService;
         this.clienteService = clienteService;
     }
 
-    public void cadastrarPessoaFisica(String endereco,String nome, String telefone, String cpf) {
+    public void cadastrarPessoaFisica(String nome,String endereco, String telefone, String cpf) {
         PessoaFisica pf = new PessoaFisica(nome, endereco, telefone, cpf);
         clienteService.cadastrarCliente(pf);
     }
