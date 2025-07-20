bb5aa3e6e1b2cbf214c93019ac295eb54b85e8af
yanrsa-dev <yanrochadesa10@gmail.com>
2025-07-20 12:44:41 -0300

    Update ClienteRepository.java

--- repository/ClienteRepository.java
+++ repository/ClienteRepository.java
@@ -1,6 +1,7 @@
 package repository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import model.cliente.Cliente;
 
@@ -14,5 +15,18 @@ public class ClienteRepository {
   public List<Cliente> getClientes(){
     return clientes;
   }
+   public Optional<Cliente> buscarClientePorId(Long id) {
+        return clientes.stream().filter(c -> c.getId() == id).findFirst();
+    }
 
 }
+public boolean atualizarCliente(Cliente clienteAtualizado) {
+        for (int i = 0; i < clientes.size(); i++) {
+            if (clientes.get(i).getId() == clienteAtualizado.getId()) {
+                clientes.set(i, clienteAtualizado);
+                return true;
+            }
+        }
+        return false;
+    }
+
