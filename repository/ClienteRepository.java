aa44633ce53baab53dfbec8436bad5ca7588d747
yanrsa-dev <yanrochadesa10@gmail.com>
2025-07-22 19:31:21 -0300

    ArrayList->Array ClienteRepository.java

--- repository/ClienteRepository.java
+++ repository/ClienteRepository.java
@@ -1,24 +1,35 @@
 package repository;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import model.cliente.Cliente;
 
 public class ClienteRepository {
-  private List<Cliente> clientes = new ArrayList<>();
+  private Cliente[] clientes= new Cliente[tamanhoAtual];
+  private int tamanhoAtual=0;
   private static Long contador=1L;
   
   public void cadastraCliente(Cliente cliente){
-    clientes.add(cliente);
+    if(tamanhoAtual == clientes.length){
+        Cliente[] novoArray = new Cliente[clientes.length + 10];
+        System.arraycopy(clientes,0,novoArray,0,clientes.length);
+        clientes = novoArray;
+    }
     cliente.setId(contador++);
+    clientes[tamanhoAtual++] = cliente;
   }
 
-  public List<Cliente> getClientes(){
-    return clientes;
+  public Cliente[] getClientes(){
+    Cliente[] clientesAtuais = new Cliente[tamanhoAtual];
+     System.arraycopy(clientes,0,clientesAtuais,0,clientes.length);
+    return clientesAtuais;
   }
    public Optional<Cliente> buscarClientePorId(Long id) {
-        return clientes.stream().filter(c -> c.getId() == id).findFirst();
+         for (int i = 0; i < tamanhoAtual; i++) {
+        if (clientes[i].getId().equals(id)) {
+            return Optional.of(clientes[i]);
+        }
+    }
+    return Optional.empty();
     }
 
 
@@ -32,4 +43,3 @@ public boolean atualizarCliente(Cliente clienteAtualizado) {
         return false;
 }
 }
-
