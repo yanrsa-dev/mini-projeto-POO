f9f6da28178ac8c75cea836c61b96430cfdbf71f
yanrsa-dev <yanrochadesa10@gmail.com>
2025-07-22 19:28:57 -0300

    Update ProdutoRepository.java

--- repository/ProdutoRepository.java
+++ repository/ProdutoRepository.java
@@ -1,27 +1,36 @@
 package repository;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import model.product.Produto;
 
-public class ProdutoRepository {
-  private List<Produto> produtos = new ArrayList<>();
+public class ProdutoRepository { 
+  private Produto[] produtos= new Produto[tamanhoAtual];
+  private int tamanhoAtual=0;
   private static Long contador=1L;
   
   public void adicionaProduto(Produto produto){
+    if(tamanhoAtual == produtos.length){
+        Produto[] novoArray = new Produto[produtos.length + 10];
+        System.arraycopy(produtos,0,novoArray,0,produtos.length);
+        produtos = novoArray;
+    }
     produto.setId(contador++);
-    produtos.add(produto);
+    produtos[tamanhoAtual++] = produto;
   }
 
-  public List<Produto> listar(){
-    return produtos;
+  public Produto[] listar(){
+     Produto[] produtosAtuais = new Produto[tamanhoAtual];
+     System.arraycopy(produtos,0,produtosAtuais,0,produtos.length);
+    return produtosAtuais;
   }
 
   public Optional<Produto> getProdutoById(Long id){
-    return produtos.stream()
-      .filter(p -> p.getId().equals(id))
-      .findFirst();
-  }
+for (int i = 0; i < tamanhoAtual; i++) {
+        if (produtos[i].getId().equals(id)) {
+            return Optional.of(produtos[i]);
+        }
+    }
+    return Optional.empty();
 
 }
+
